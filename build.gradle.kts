917cc06f931d0049ede054085ceb3faf6675c08f
Anže Sodja <asodja@gradle.com>
2023-06-15 20:22:24 +0200

    Show that also other methods work with gradle-profiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,9 @@
 println("Value of myGradleProperty=${project.providers.gradleProperty("myGradleProperty").getOrElse(null)}")
 println("Value of mySystemProperty=${project.providers.systemProperty("mySystemProperty").getOrElse(null)}")
+
+// This will blow up if no default value is set
+val myGradleProperty: String by project.properties
+println("Value of myGradleProperty: String by project.properties=${myGradleProperty}")
+// This will blow up if no default value is set
+println("Value of project.property(\"myGradleProperty\")=${project.property("myGradleProperty").toString()}")
+println("Value of properties[\"myGradleProperty\"]=${properties["myGradleProperty"]}")
